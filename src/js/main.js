59b0bc1c35f9b68b78b7ba9a01587d84e99aaceb
Oleg <stroleg912@gmail.com>
2022-10-22 22:30:31 +0300

    change

--- src/js/main.js
+++ src/js/main.js
@@ -128,3 +128,261 @@ if (allMenuItem) {
   });
   // });
 }
+
+//меню фильтров
+const menuFiltr = document.querySelectorAll('.menu-sidebar__dropdown');
+const menuFiltrIcon = document.querySelectorAll('.menu-sidebar__icon');
+const menuFiltrSubmenu = document.querySelectorAll('.menu-sidebar__submenu');
+console.log(menuFiltr);
+console.log(menuFiltrSubmenu);
+
+if (menuFiltr) {
+  // перебираем по очереди пункты меню фильтров
+  menuFiltr.forEach(function (item, index) {
+    console.log(item);
+    // слушаем выбраный пункт меню
+    item.addEventListener('click', function (element) {
+
+      menuFiltrIcon.forEach(function (item, i) {
+        if (index == i) {
+          item.classList.toggle('_rotate');
+        }
+      });
+
+
+      console.log('Кликаю');
+      menuFiltrSubmenu.forEach(function (item, i) {
+        if (index == i) {
+          item.classList.toggle('_active');
+        }
+      })
+    })
+  })
+}
+
+//Главное меню sidebar
+const menuSbMainDrop = document.querySelectorAll('.menu-sb-main__dropdown');
+const menuSbMain = document.querySelectorAll('.menu-sb-main__category');
+const menuSbIcon = document.querySelectorAll('.menu-sb-main__icon');
+const menuSbItem = document.querySelectorAll('.menu-sb-main__item');
+// console.log(menuSbMain);
+if (menuSbMainDrop) {
+
+  menuSbMainDrop.forEach(function (item, index) {
+    item.addEventListener('click', function () {
+
+      menuSbItem.forEach(function (item, i) {
+        if (index == i) {
+          item.classList.toggle('_bg');
+        }
+
+      });
+      console.log('Click');
+      item.classList.toggle('_bg');
+      menuSbIcon.forEach(function (item, i) {
+        if (index == i) {
+          item.classList.toggle('_rotate');
+        }
+      });
+
+      menuSbMain.forEach(function (item, i) {
+        if (index == i) {
+          item.classList.toggle('_active');
+
+        }
+
+      });
+    });
+  });
+
+}
+// Подменю главного меню sidebar
+const menuSbMainSubCat = document.querySelectorAll('.menu-sb-main__subcategory');
+const menuSbMainCatDrop = document.querySelectorAll('.menu-sb-main__category-dropdown');
+const menuSbMainIcon = document.querySelectorAll('.menu-sb-main__category-icon');
+console.log(menuSbMainCatDrop);
+if (menuSbMainCatDrop) {
+
+  menuSbMainCatDrop.forEach(function (item, index) {
+    console.log(index);
+    item.addEventListener('click', function (e) {
+
+      menuSbMainIcon.forEach(function (item, i) {
+
+        if (index == i) {
+          item.classList.toggle('_rotate');
+        }
+
+      });
+
+      console.log('Click');
+
+      menuSbMainSubCat.forEach(function (item, i) {
+        console.log(index);
+        console.log(i);
+        if (index == i) {
+          item.classList.toggle('_active');
+        }
+      });
+    });
+  });
+}
+
+//Переключение между сеткой и списком
+const srtList = document.getElementById('srt-list');
+const srtGrid = document.getElementById('srt-grid');
+const product = document.querySelectorAll('.product');
+const productLink = document.querySelectorAll('.product__link');
+const productImage = document.querySelectorAll('.product__image');
+const productContainer = document.querySelectorAll('.product__container');
+const productName = document.querySelectorAll('.product__name');
+const productBody = document.querySelectorAll('.product__body');
+const productText = document.querySelectorAll('.product__text');
+const productCount = document.querySelectorAll('.product__count');
+const productPrice = document.querySelectorAll('.product__price');
+if (srtList) {
+  srtList.addEventListener('change', function (e) {
+    onchange = function (e) {
+
+      product.forEach(function (item) {
+        item.classList.add('_row');
+      });
+
+      productLink.forEach(function (item) {
+        item.classList.add('_row');
+      });
+
+      productImage.forEach(function (item) {
+        item.classList.add('_row');
+      });
+
+      productContainer.forEach(function (item) {
+        item.classList.add('_row');
+      });
+
+      productName.forEach(function (item) {
+        item.classList.add('_row');
+      });
+
+      productBody.forEach(function (item) {
+        item.classList.add('_row');
+      });
+
+      productText.forEach(function (item) {
+        item.classList.add('_row');
+      });
+
+      productCount.forEach(function (item) {
+        item.classList.add('_row');
+      });
+
+      productPrice.forEach(function (item) {
+        item.classList.add('_row');
+      });
+
+    };
+
+    if (srtGrid) {
+      srtGrid.addEventListener('change', function (e) {
+        onchange = function (e) {
+          console.log('Tect');
+          product.forEach(function (item) {
+            item.classList.remove('_row');
+          });
+
+          productLink.forEach(function (item) {
+            item.classList.remove('_row');
+          });
+
+          productImage.forEach(function (item) {
+            item.classList.remove('_row');
+          });
+
+          productContainer.forEach(function (item) {
+            item.classList.remove('_row');
+          });
+
+          productName.forEach(function (item) {
+            item.classList.remove('_row');
+          });
+
+          productBody.forEach(function (item) {
+            item.classList.remove('_row');
+          });
+
+          productText.forEach(function (item) {
+            item.classList.remove('_row');
+          });
+
+          productCount.forEach(function (item) {
+            item.classList.remove('_row');
+          });
+
+          productPrice.forEach(function (item) {
+            item.classList.remove('_row');
+          });
+        };
+      });
+    }
+    // console.log(list.checked);
+    // console.log(onchange);
+    // if (list.checked == true) {
+    //   product.forEach(function (item) {
+    //     item.classList.add('_row');
+    //   });
+    //   productLink.forEach(function (item) {
+    //     item.classList.add('_row');
+    //   });
+    //   productImage.forEach(function (item) {
+    //     item.classList.add('_row');
+    //   });
+    //   productContainer.forEach(function (item) {
+    //     item.classList.add('_row');
+    //   });
+    //   productName.forEach(function (item) {
+    //     item.classList.add('_row');
+    //   });
+    //   productBody.forEach(function (item) {
+    //     item.classList.add('_row');
+    //   });
+    //   productText.forEach(function (item) {
+    //     item.classList.add('_row');
+    //   });
+    //   productCount.forEach(function (item) {
+    //     item.classList.add('_row');
+    //   });
+    //   productPrice.forEach(function (item) {
+    //     item.classList.add('_row');
+    //   });
+    // } else {
+    //   product.forEach(function (item) {
+    //     item.classList.remove('_row');
+    //   });
+    //   productLink.forEach(function (item) {
+    //     item.classList.remove('_row');
+    //   });
+    //   productImage.forEach(function (item) {
+    //     item.classList.remove('_row');
+    //   });
+    //   productContainer.forEach(function (item) {
+    //     item.classList.remove('_row');
+    //   });
+    //   productName.forEach(function (item) {
+    //     item.classList.remove('_row');
+    //   });
+    //   productBody.forEach(function (item) {
+    //     item.classList.remove('_row');
+    //   });
+    //   productText.forEach(function (item) {
+    //     item.classList.remove('_row');
+    //   });
+    //   productCount.forEach(function (item) {
+    //     item.classList.remove('_row');
+    //   });
+    //   productPrice.forEach(function (item) {
+    //     item.classList.remove('_row');
+    //   });
+    // }
+
+  });
+}
