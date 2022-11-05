f244ec908ae2dd44f3ded22851c30cb8159d42bf
Oleg <stroleg912@gmail.com>
2022-11-05 20:36:36 +0300

    change

--- src/js/main.js
+++ src/js/main.js
@@ -458,6 +458,7 @@ const swiper3 = new Swiper(".modal-swiper", {
   modules: [Navigation, Pagination, Thumbs],
   spaceBetween: 0,
   slidesPerView: 1,
+  autoHeight: true,
   pagination: {
     el: ".swiper-pagination",
     type: "fraction",
@@ -493,3 +494,162 @@ if (slide) {
     });
   });
 }
+
+// ZOOM Увелечительное стекло
+function magnify(imgID, zoom) {
+  let img, glass, w, h, bw;
+  // img = document.querySelector(imgID);
+  img = imgID;
+
+  /* Создать увеличительное стекло: */
+  glass = document.createElement("DIV");
+  glass.setAttribute("class", "img-magnifier-glass");
+  /* Вставить увеличительное стекло: */
+  img.parentElement.insertBefore(glass, img);
+
+  /* Установите свойства фона для стекла лупы: */
+  glass.style.backgroundImage = "url('" + img.src + "')";
+  glass.style.backgroundRepeat = "no-repeat";
+  glass.style.backgroundSize = (img.width * zoom) + "px " + (img.height * zoom) + "px";
+  bw = 3;
+  w = glass.offsetWidth / 2;
+  h = glass.offsetHeight / 2;
+
+  /* Выполните функцию, когда кто-то перемещает лупу по изображению: */
+  glass.addEventListener("mousemove", moveMagnifier);
+  img.addEventListener("mousemove", moveMagnifier);
+
+  /* а также для сенсорных экранов: */
+  glass.addEventListener("touchmove", moveMagnifier);
+  img.addEventListener("touchmove", moveMagnifier);
+  glass.addEventListener("click", function () {
+    glass.setAttribute("class", "_hidden");
+  });
+
+
+  function moveMagnifier(e) {
+    var pos, x, y;
+    /* Предотвратите любые другие действия, которые могут возникнуть при перемещении по изображению */
+    e.preventDefault();
+    /* Получить позиции курсора x и y: */
+    pos = getCursorPos(e);
+    x = pos.x;
+    y = pos.y;
+    /* Не допускайте, чтобы лупа находилась вне изображения: */
+    if (x > img.width - (w / zoom)) {
+      x = img.width - (w / zoom);
+    }
+    if (x < w / zoom) {
+      x = w / zoom;
+    }
+    if (y > img.height - (h / zoom)) {
+      y = img.height - (h / zoom);
+    }
+    if (y < h / zoom) {
+      y = h / zoom;
+    }
+    /* Установите положение стекла лупы: */
+    glass.style.left = (x - w) + "px";
+    glass.style.top = (y - h) + "px";
+    /* Покажите, что такое увеличительное стекло "смотреть": */
+    glass.style.backgroundPosition = "-" + ((x * zoom) - w + bw) + "px -" + ((y * zoom) - h + bw) + "px";
+  }
+
+  function getCursorPos(e) {
+    var a, x = 0,
+      y = 0;
+    e = e || window.event;
+    /* Получить x и y позиции изображения: */
+    a = img.getBoundingClientRect();
+    /* Вычислите координаты курсора x и y относительно изображения: */
+    x = e.pageX - a.left;
+    y = e.pageY - a.top;
+    /* Consider any page scrolling: */
+    x = x - window.pageXOffset;
+    y = y - window.pageYOffset;
+    return {
+      x: x,
+      y: y
+    };
+  }
+}
+
+const myImg = document.querySelectorAll('.modal-slider__img');
+// let loupe = document.querySelector('.modal-slider__loupe');
+
+
+myImg.forEach(function (item) {
+  item.addEventListener('click', function () {
+    // magnify(item, 2);
+    // item.classList.toggle('_active');
+    item.classList.toggle('_active');
+    magnify(item, 3);
+
+  });
+  // item.addEventListener('mouseout', function () {
+  //   const boolean = item.classList.toggle('_active');
+  //   console.log('TEST');
+  //   // magnify(item, 0);
+  // });
+});
+const yes = true;
+const no = false;
+// console.log(loupe);
+// const myGlass = document.querySelector('.img-magnifier-glass');
+// loupe.addEventListener('click', function () {
+// const glass = document.createElement("DIV");
+// myImg.forEach(function (item) {
+// console.log(item);
+// loupe.addEventListener('click', function glass() {
+// console.log(loupe.classList.toggle('_active'));
+// if (loupe.classList.toggle('_active')) {
+// magnify(item, 2, yes);
+// console.log('ДА');
+// loupe.removeEventListener('click', glass);
+// } else {
+// console.log('НЕТ');
+
+// magnify(item, 2, no);
+// const hidden = '_hidden';
+// magnify(item, 2);
+// }
+// loupe.classList.toggle('_active');
+// console.log(item);
+// console.log(glass);
+// glass.setAttribute("class", "img-magnifier-glass");
+// magnify(item, 2);
+// item.addEventListener('mouseout', function () {
+//   console.log('TEST');
+//   // magnify(item, 2, active);
+// });
+// console.log(myGlass);
+// loupe.removeEventListener('click', glasss);
+// });
+// });
+// console.log('Test');
+// glass.classList.toggle('_active');
+// });
+
+// loupe.addEventListener('click', function glass() {
+// console.log(loupe.classList.toggle('_active'));
+// if (loupe.classList.toggle('_active')) {
+// myImg.forEach(function (item, i) {
+// console.log(item);
+// if (i == 0) {
+// magnify(item, 2);
+// } else if (i == 1) {
+// magnify(item, 2);
+// }
+// magnify(item, 2)
+// });
+// magnify(item, 2, yes);
+// console.log('ДА');
+// loupe.removeEventListener('click', glass);
+// } else {
+// console.log('НЕТ');
+
+// magnify(item, 2, no);
+// const hidden = '_hidden';
+// magnify(item, 2);
+// }
+// });
